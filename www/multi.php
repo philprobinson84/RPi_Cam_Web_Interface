fee1fd77602d255b542217cc342acc4e6f3be80e
Phil Robinson <philip.robinson84@gmail.com>
2016-07-03 18:17:45 +0100

    changed order of multi

--- www/multi.php
+++ www/multi.php
@@ -14,7 +14,7 @@ require_once(BASE_DIR.'/header.php');
         </div>
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 full-size">
           <div class="h_iframe">
-            <iframe width="2" height="2" src="http://sanamu.co.uk:81" frameborder="0" allowfullscreen="" scrolling="no"></iframe>
+            <iframe width="2" height="2" src="http://sanamu.co.uk:83" frameborder="0" allowfullscreen="" scrolling="no"></iframe>
           </div>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 full-size">
@@ -24,7 +24,7 @@ require_once(BASE_DIR.'/header.php');
         </div>
         <div class="col-xs-12 col-sm-12 col-md-6 col-lg-6 full-size">
           <div class="h_iframe">
-            <iframe width="2" height="2" src="http://sanamu.co.uk:83" frameborder="0" allowfullscreen="" scrolling="no"></iframe>
+            <iframe width="2" height="2" src="http://sanamu.co.uk:81" frameborder="0" allowfullscreen="" scrolling="no"></iframe>
           </div>
         </div>
       </div>
